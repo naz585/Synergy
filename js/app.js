3723f0a5fa90563b76dc9228384701bd76ff8620
Riley Griffin <rigriffin241@gmail.com>
2018-07-30 19:32:36 -0400

    made fetch song page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+$(document).ready(function() {
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=34348eae6672b6e0bff528a9068359fa&limit=10&format=json&callback=?", function(json) {
+        var html = '';
+        $.each(json.topartists.artist, function(i, item) {
+            html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>";
+        });
+        $('#result').append(html);
+    });
+});
