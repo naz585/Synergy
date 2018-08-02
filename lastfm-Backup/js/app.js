179b70605f3826fe8889cb62e82e1cee35382b33
Riley Griffin <rigriffin241@gmail.com>
2018-08-01 23:19:13 -0400

    made search page for the app.

--- lastfm-Backup/js/app.js
+++ lastfm-Backup/js/app.js
@@ -31,7 +31,7 @@ const goAlbums = () => {
       $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&user=test&api_key=34348eae6672b6e0bff528a9068359fa&limit=10&format=json&callback=?", function(json) {
           var html = '';
           $.each(json.topalbums.album, function(i, item) {
-              html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " + item.playcount + "</a></p>" + "<img>" + item.artwork + "</img>";
+              html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " + item.playcount + "</a></p>" + "<img>" + item.image.small + "</img>";
           });
           $('#albums').append(html);
       });
@@ -53,7 +53,7 @@ const goSearch = () => {
     $.getJSON(fmurl, function(data) {
       var html = '';
       $.each(data.results.trackmatches.track, function(i, item) {
-          html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>" + "<img>" + item.artwork + "</img>";
+          html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>" + "<img>" + item.image.medium + "</img>";
       });
       $('#results').append(html);
       console.log(fmurl);
