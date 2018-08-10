dc724fee8eeda1429a614869339279fef6a959df
naz585 <naz585@aol.com>
2018-08-09 20:18:50 -0400

    removed a folder

--- musikbot/react-pusher/src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: 'musikbot.auth0.com',
-    clientId: '8zR786vP4_hPdHq7LD0XIRTiGacWTG0r',
-    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
-}
